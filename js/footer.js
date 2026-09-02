867c4ea1c4d9bb3874a6294f3749f0ca6b78c731
esramos-design <52668954+esramos-design@users.noreply.github.com>
2026-09-02 21:09:11 +0100

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
       <div class="widget-meta">
         <b>Virtual NATO Flight Planning Suite</b><br>
         Property of Virtual NATO &copy; 2017–2026. All rights reserved.<br>
-        Version 5.0.6 - ALPHA
+        Version 5.0.6 - LIVE
       </div>
     `;
   }
