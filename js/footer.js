18374518bb86020bdacdae9089c11984feb5c337
esramos-design <52668954+esramos-design@users.noreply.github.com>
2026-09-06 10:35:32 +0100

    Add files via upload

--- js/footer.js
+++ js/footer.js
@@ -1,10 +1,2 @@
-/* vNATO V5.3 TEAM LIVE - obfuscated: footer.js */
-(function(){
-var _0xv=['LyoqCiAqIFZpcnR1YWwgTkFUTyBGbGlnaHQgUGxhbm5pbmcgU3VpdGUgLSBGb290ZXIgQ29tcG9uZW50CiAqIElzb2xhdGUgYW5kIG1hbmFnZSBmb290ZXIgbWV0YWRhdGEsIHZlcnNpb25zLCBhbmQgbGlua3MgaGVyZS4KICovCmRvY3Vt','ZW50LmFkZEV2ZW50TGlzdGVuZXIoIkRPTUNvbnRlbnRMb2FkZWQiLCBmdW5jdGlvbigpIHsKICBjb25zdCBmb290ZXJDb250YWluZXIgPSBkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgid2lkZ2V0Rm9vdGVyQ29udGFpbmVyIik7CiAgaWYg','KGZvb3RlckNvbnRhaW5lcikgewogICAgZm9vdGVyQ29udGFpbmVyLmlubmVySFRNTCA9IGAKICAgICAgPGRpdiBjbGFzcz0id2lkZ2V0LW1ldGEiPgogICAgICAgIDxiPlZpcnR1YWwgTkFUTyBGbGlnaHQgUGxhbm5pbmcgU3VpdGU8L2I+','PGJyPgogICAgICAgIFByb3BlcnR5IG9mIFZpcnR1YWwgTkFUTyAmY29weTsgMjAxN+KAkzIwMjYuIEFsbCByaWdodHMgcmVzZXJ2ZWQuPGJyPgogICAgICAgIFZlcnNpb24gNS4wLjggLSBBTFBIQSBCVUlMRAogICAgICA8L2Rpdj4KICAg','IGA7CiAgfQp9KTsK'];
-var _0xb=_0xv.join("");
-var _0xs=atob(_0xb);
-var _0xa=new Uint8Array(_0xs.length);
-for(var _0xi=0;_0xi<_0xs.length;_0xi++){_0xa[_0xi]=_0xs.charCodeAt(_0xi);}
-var _0xc=new TextDecoder("utf-8").decode(_0xa);
-(0,eval)(_0xc);
-})();
+/* vNATO V5.4 TEAM LIVE - obfuscated: footer.js */
+(()=>{const _p=["LyoqCiAqIFZpcnR1YWwgTkFUTyBGbGlnaHQgUGxhbm5pbmcgU3VpdGUgLSBGb290ZXIgQ29tcG9uZW50CiAqIElzb2xhdGUgYW5kIG1hbmFnZSBmb290ZXIgbWV0YWRhdGEsIHZlcnNpb25zLCBhbmQgbGlua3MgaGVyZS4KICovCmRvY3Vt","ZW50LmFkZEV2ZW50TGlzdGVuZXIoIkRPTUNvbnRlbnRMb2FkZWQiLCBmdW5jdGlvbigpIHsKICBjb25zdCBmb290ZXJDb250YWluZXIgPSBkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgid2lkZ2V0Rm9vdGVyQ29udGFpbmVyIik7CiAgaWYg","KGZvb3RlckNvbnRhaW5lcikgewogICAgZm9vdGVyQ29udGFpbmVyLmlubmVySFRNTCA9IGAKICAgICAgPGRpdiBjbGFzcz0id2lkZ2V0LW1ldGEiPgogICAgICAgIDxiPlZpcnR1YWwgTkFUTyBGbGlnaHQgUGxhbm5pbmcgU3VpdGU8L2I+","PGJyPgogICAgICAgIFByb3BlcnR5IG9mIFZpcnR1YWwgTkFUTyAmY29weTsgMjAxN+KAkzIwMjYuIEFsbCByaWdodHMgcmVzZXJ2ZWQuPGJyPgogICAgICAgIFZlcnNpb24gNS4wLjggLSBBTFBIQSBCVUlMRAogICAgICA8L2Rpdj4KICAg","IGA7CiAgfQp9KTsK"].join("");const _b=atob(_p);const _u=new Uint8Array(_b.length);for(let i=0;i<_b.length;i++)_u[i]=_b.charCodeAt(i);(0,eval)(new TextDecoder().decode(_u));})();
